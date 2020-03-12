1b4e5ed853f89747a1fc9479023fe03b48aecd41
Uros Obucina <uros.obucina@webpro.rs>
2020-03-12 19:38:19 +0100

    Readme corrected

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - Download app into your localhost folder.
 - Import citrus_fruits.sql into your database.
 - You are ready to go. 
+- To access admin => login.php (username:Uros,password:uros123)
--- services/ajax-services.php
+++ services/ajax-services.php
@@ -4,7 +4,7 @@ require("database.php");
 function comment(){
     $conn = connect();
     $sql = "INSERT INTO comments (status,name, email, text,date) VALUES (?,?,?,?,?)";
-    if(preg_match('/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/',$_POST['email']) && preg_match('/^\w{3,40}.$/',$_POST['subject'])){
+    if(preg_match('/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/',$_POST['email']) && preg_match('/^[A-Z][a-z\s]{3,40}.$/',$_POST['subject'])){
         $conn->prepare($sql)->execute([0,$_POST['name'], $_POST['email'], $_POST['subject'],$_POST['date']]);
     }else{
         echo "Inser your data again!";
